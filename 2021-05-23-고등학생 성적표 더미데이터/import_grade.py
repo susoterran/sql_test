c073ca0c644b5c79d25452ba7325422f8f72c4b9
susoterran <susoterran@naver.com>
2022-03-30 08:43:44 +0900

    오류 수정

--- 2021-05-23-고등학생 성적표 더미데이터/import_grade.py
+++ 2021-05-23-고등학생 성적표 더미데이터/import_grade.py
@@ -11,7 +11,7 @@ from faker import Faker
 fake = Faker('ko_KR')
 
 ## MySQL Connection 연결
-conn = pymysql.connect(host='127.0.0.1', user='faker', password='패스워드', db='data',
+conn = pymysql.connect(host='192.168.10.130', user='root', password='asdf1234', db='data',
                     charset='utf8')
 
 ## Connection 으로부터 Cursor 생성
@@ -69,7 +69,7 @@ def import_grade():
 
 if __name__ == '__main__':
 
-	excel_path = "high.xlsx"
+    excel_path = "high.xlsx"
     sheet_name = "Sheet1"	    
 	
 	#import_school(excel_path, sheet_name)
@@ -77,25 +77,25 @@ if __name__ == '__main__':
 	#import_grade()
 	
 
-	while 1:
-    print('############ Excel Import Manager ##########')
-    print('#### tb_area, tb_school, tb_affiliation 테이블이 있는지 확인해주세요')
-    print('1. 학교 데이터를 엑셀에서 불러와 추가합니다')
-    print('2. 학생 데이터를 faker로 생성하여 추가합니다.')
-    print('3. 성적 데이터를 faker로 생성하여 추가합니다.')
-    print('4. 종료')
-    menu = int(input('메뉴를 선택하세요 : '))
-    excel_path = "high.xlsx"
-    sheet_name = "Sheet1"
+    while 1:
+        print('############ Excel Import Manager ##########')
+        print('#### tb_area, tb_school, tb_affiliation 테이블이 있는지 확인해주세요')
+        print('1. 학교 데이터를 엑셀에서 불러와 추가합니다')
+        print('2. 학생 데이터를 faker로 생성하여 추가합니다.')
+        print('3. 성적 데이터를 faker로 생성하여 추가합니다.')
+        print('4. 종료')
+        menu = int(input('메뉴를 선택하세요 : '))
+        excel_path = "C:\\Users\\unknown\\Documents\\git_test\\sql_test\\sql_test\\2021-05-23-고등학생 성적표 더미데이터\\high.xlsx"
+        sheet_name = "Sheet1"
 
-    if menu == 1:
-        import_school(excel_path, sheet_name)
-    elif menu == 2:
-        import_student()
-    elif menu == 3:
-        import_grade()
-    elif menu == 4:
-        #conn.close()
-        break
-    else :
-         print("없는 메뉴입니다. 다시 입력해주세요.")    
+        if menu == 1:
+            import_school(excel_path, sheet_name)
+        elif menu == 2:
+            import_student()
+        elif menu == 3:
+            import_grade()
+        elif menu == 4:
+            #conn.close()
+            break
+        else :
+            print("없는 메뉴입니다. 다시 입력해주세요.")    
